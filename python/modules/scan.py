51e433595b89e33510bc39722ce93a2c9aa12a6b
Sara04 <sara.sedlar@gmail.com>
2017-09-11 22:51:53 +0200

    [scan]: remove brain masks and tumor distance maps computation from scan class

--- python/modules/scan.py
+++ python/modules/scan.py
@@ -2,7 +2,6 @@
 import os
 import nibabel as nib
 import numpy as np
-from scipy.ndimage import morphology
 
 
 class ScanBRATS(object):
@@ -15,10 +14,6 @@ class ScanBRATS(object):
             mode: train, valid or test mode (dataset)
 
         Methods:
-            compute_brain_mask: find binary mask that corresponds to the
-                brain and skul region
-            compute_tumor_distance_map: compute distances of healthy
-                tissue voxels to tumorous tissue voxels
             load_volume: load a scan of chosen modality
             load_normalized_volume: load scan of a chosen modality with
                 normalized values
@@ -32,57 +27,6 @@ class ScanBRATS(object):
         self.name = name
         self.relative_path = relative_path
         self.mode = mode
-
-    def _compute_and_save_brain_mask(self, db, brain_mask_path):
-
-        v_t1 = self.load_volume(db, 't1')
-        v_t2 = self.load_volume(db, 't2')
-        v_flair = self.load_volume(db, 'flair')
-        v_t1ce = self.load_volume(db, 't1ce')
-
-        bm = (v_t1 != 0) * (v_t2 != 0) * (v_flair != 0) * (v_t1ce != 0)
-        bm.tofile(brain_mask_path)
-
-    def compute_brain_mask(self, db, exp_out):
-        """Compute brain mask."""
-        """
-            Arguments:
-                db: DatabaseBRATS
-                exp_out: experiment output for meta data
-        """
-        bm_path = os.path.join(exp_out, self.name + '_brain_mask.bin')
-        self._compute_and_save_brain_mask(db, bm_path)
-
-    def _compute_and_save_tumor_distance_map(self, db, brain_mask,
-                                             tumor_distance_map_path):
-
-        v_seg = self.load_volume(db, 'seg')
-        seg_mask = v_seg != 0
-        struct_elem = np.ones((3, 3, 3))
-        v = 1.0
-
-        tumor_dist_map = np.zeros((db.h, db.w, db.d))
-        while(np.sum(seg_mask) != db.w * db.h * db.d):
-            seg_mask_d = morphology.binary_dilation(seg_mask, struct_elem)
-            tumor_dist_map += (seg_mask_d - seg_mask) * v
-            seg_mask = np.copy(seg_mask_d)
-            v += 1
-
-        tumor_dist_map *= brain_mask
-        tumor_dist_map = np.clip(tumor_dist_map, a_min=0.0, a_max=255.0)
-        tumor_dist_map.astype('uint8').tofile(tumor_distance_map_path)
-
-    def compute_tumor_distance_map(self, db, bm_exp_out, tdm_exp_out):
-        """Tumor distance map computation."""
-        """
-            Arguments:
-                db: DatabaseBRATS
-                exp_out: experiment output for meta data
-        """
-        tdm_path = os.path.join(tdm_exp_out, self.name + '_tumor_dist.bin')
-
-        brain_mask = self.load_brain_mask(bm_exp_out)
-        self._compute_and_save_tumor_distance_map(db, brain_mask, tdm_path)
 
     def load_volume(self, db, m):
         """Loading volume as numpy array."""
